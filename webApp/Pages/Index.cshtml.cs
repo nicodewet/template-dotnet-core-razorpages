1fb0443f78f4c6de54fc471896081aeb0824f4f4
Nico de Wet <nico@thorgil.com>
2020-10-28 11:59:32 +1300

    initial Razor Pages design

--- webApp/Pages/Index.cshtml.cs
+++ webApp/Pages/Index.cshtml.cs
@@ -5,21 +5,41 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Currency.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace webApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ICurrencyWordsService _currencyWordsService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty]
+        [MinLength(1), MaxLength(60)]
+        [Required]
+        public string Name { get; set; }
+
+        [BindProperty]
+        [Range(0.00, 999.99)]
+        [DataType(DataType.Currency)]
+        public decimal? Number { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, ICurrencyWordsService currencyWordsService)
         {
             _logger = logger;
+            _currencyWordsService = currencyWordsService;
         }
-
         public void OnGet()
         {
-
         }
+        public void OnPost()
+        {
+            ViewData["name"] = Name;
+            if(Number.HasValue)
+            {
+                ViewData["number"] = _currencyWordsService.toWordsInDollarsAndCents(Number.Value);
+            }
+        }
+
     }
 }
