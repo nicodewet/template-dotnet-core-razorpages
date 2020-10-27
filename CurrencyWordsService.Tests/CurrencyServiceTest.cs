9a6f31056a7a9427742c4851db90ad44f47a3e6c
Nico de Wet <nico@thorgil.com>
2020-10-27 20:14:53 +1300

    adding single digit support in dollars and cents

--- CurrencyWordsService.Tests/CurrencyServiceTest.cs
+++ CurrencyWordsService.Tests/CurrencyServiceTest.cs
@@ -12,7 +12,27 @@ namespace Currency.Services.Tests
         }
 
        [Theory]
-       [InlineData("0", "ZERO DOLLARS")]
+       [InlineData("0", "ZERO DOLLARS AND ZERO CENTS")]
+       [InlineData("0.00", "ZERO DOLLARS AND ZERO CENTS")]
+       [InlineData("0.01", "ZERO DOLLARS AND ONE CENT")]
+       [InlineData("0.02", "ZERO DOLLARS AND TWO CENTS")]
+       [InlineData("0.03", "ZERO DOLLARS AND THREE CENTS")]
+       [InlineData("0.04", "ZERO DOLLARS AND FOUR CENTS")]
+       [InlineData("0.05", "ZERO DOLLARS AND FIVE CENTS")]
+       [InlineData("0.06", "ZERO DOLLARS AND SIX CENTS")]
+       [InlineData("0.07", "ZERO DOLLARS AND SEVEN CENTS")]
+       [InlineData("0.08", "ZERO DOLLARS AND EIGHT CENTS")]
+       [InlineData("0.09", "ZERO DOLLARS AND NINE CENTS")]
+       [InlineData("1.00", "ONE DOLLAR AND ZERO CENTS")]
+       [InlineData("1.01", "ONE DOLLAR AND ONE CENT")]
+       [InlineData("1.02", "ONE DOLLAR AND TWO CENTS")]
+       [InlineData("1.03", "ONE DOLLAR AND THREE CENTS")]
+       [InlineData("1.04", "ONE DOLLAR AND FOUR CENTS")]
+       [InlineData("1.05", "ONE DOLLAR AND FIVE CENTS")]
+       [InlineData("1.06", "ONE DOLLAR AND SIX CENTS")]
+       [InlineData("1.07", "ONE DOLLAR AND SEVEN CENTS")]
+       [InlineData("1.08", "ONE DOLLAR AND EIGHT CENTS")]
+       [InlineData("1.09", "ONE DOLLAR AND NINE CENTS")]
        public void ToWords(string value, string expected)
        {
            decimal number = Convert.ToDecimal(value);
--- CurrencyWordsService/CurrencyWordsService.cs
+++ CurrencyWordsService/CurrencyWordsService.cs
@@ -5,39 +5,79 @@ namespace Currency.Services
 {
     public class CurrencyWordsService : ICurrencyWordsService
     {
-        private static IReadOnlyDictionary<int, string> _intToText = new Dictionary<int, string> {
-            {0, "zero"}
+        private static IReadOnlyDictionary<int, string> _digitToText = new Dictionary<int, string> {
+            {0, "zero"},
+            {1, "one"},
+            {2, "two"},
+            {3, "three"},
+            {4, "four"},
+            {5, "five"},
+            {6, "six"},
+            {7, "seven"},
+            {8, "eight"},
+            {9, "nine"}, 
         };
-        public static IReadOnlyDictionary<int, string> IntToText => _intToText;
+        public static IReadOnlyDictionary<int, string> DigitToText => _digitToText;
         private static readonly string DOLLAR = "dollar";
+        private static readonly string CENT = "cent";
         private static readonly string DOLLARS = DOLLAR + "s"; 
+        private static readonly string CENTS = CENT + "s";
 
         public string toWordsInDollarsAndCents(decimal candidate)
         {
             string result = "";
 
-            if (candidate == decimal.Zero)
-            {
-                IntToText.TryGetValue(0, out result);
-                result = String.Format("{0} {1}", result, DOLLARS);
+            decimal roundedCandidate = Math.Round(candidate, 2, MidpointRounding.ToEven);
 
-            } else {
+            int dollars = (int) roundedCandidate;
+            int cents = (int) ((candidate - dollars) * 100);
+            
+            string dollarsAsText = dollarsToText(dollars);
+            string centsAsText = centsToText(cents);
 
-                bool isInt = candidate % 1 == 0;
-
-                if (isInt)
-                {
-                    throw new NotImplementedException("only supporting zero at present");
-                }
-                else
-                {
-                    throw new NotImplementedException("only supporting whole numbers at present");
-                }
-
-            }
+            result = String.Format("{0} and {1}", dollarsAsText, centsAsText);
             
             return result.ToUpper();
+        }
 
+        private string dollarsToText(int dollars) {
+            if (dollars < 0) {
+                throw new ArgumentException("dollars must be positive");
+            }
+            if (dollars == 1) {
+                return String.Format("{0} {1}", positiveNumberToText(dollars), DOLLAR);
+            } else if (dollars >= 0 && dollars <= 9) {
+                return String.Format("{0} {1}", positiveNumberToText(dollars), DOLLARS);
+            } else {
+                return "";
+            }
+        }
+
+        private string centsToText(int cents) {
+            if (cents < 0 || cents > 99) {
+                throw new ArgumentException("cents must be 0 to 99");
+            }
+            if (cents == 1) {
+                return String.Format("{0} {1}", positiveNumberToText(cents), CENT);
+            } else if (cents >= 0 && cents <= 9) {
+                return String.Format("{0} {1}", positiveNumberToText(cents), CENTS);
+            } else {
+                return "";
+            }
+        }
+
+        private string positiveNumberToText(int number) {
+            string result = "";
+            if (number < 0) {
+                throw new ArgumentException("number must be positive");
+            }
+            int numberOfDigits = number.ToString().Length; 
+            if (numberOfDigits == 1) {
+                DigitToText.TryGetValue(number, out result);
+                return result; 
+            } else {
+                return result;
+            }
         }
     }
 }
